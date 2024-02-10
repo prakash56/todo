1b3b3e8cc02d78895e3ac2ee38c128065e401fa5
prakash56 <33279642+prakash56@users.noreply.github.com>
2024-02-10 13:49:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
+import Button from "./components/button";
+
+function App() {
+  return (
+    <div >
+        <Header />
+        <TodoItem />
+        <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/TodoItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoItem = () => {
+    return (
+        <li className="">
+            <input type="checkbox" />
+            <p>Eat</p>
+            <p>...</p>
+        </li>
+    );
+};
+export default TodoItem;
